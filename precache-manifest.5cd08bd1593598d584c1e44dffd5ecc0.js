09e4f79b3aa88b4bd1b3d3a15f44367ee6012529
paki_jan@yahoo.com <paki_jan@yahoo.com>
2020-01-13 11:31:44 +0500

    react testing

---
+++ precache-manifest.5cd08bd1593598d584c1e44dffd5ecc0.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "35a823259443dd807118ddcfbdc3e47c",
+    "url": "/royal/build/index.html"
+  },
+  {
+    "revision": "5edfcf79b35e145ec722",
+    "url": "/royal/build/static/css/3.903a27b1.chunk.css"
+  },
+  {
+    "revision": "13882212e0c2bce82cc6",
+    "url": "/royal/build/static/css/4.6b490189.chunk.css"
+  },
+  {
+    "revision": "fa5255f0f438d314f93d",
+    "url": "/royal/build/static/css/5.859ebdb9.chunk.css"
+  },
+  {
+    "revision": "405f8f9c81adc6800b52",
+    "url": "/royal/build/static/css/main.b22bd36b.chunk.css"
+  },
+  {
+    "revision": "c664b03c1ed9dd544dbf",
+    "url": "/royal/build/static/js/2.1ad3db09.chunk.js"
+  },
+  {
+    "revision": "81896c98bac7b5b16ab1d3790da5b937",
+    "url": "/royal/build/static/js/2.1ad3db09.chunk.js.LICENSE"
+  },
+  {
+    "revision": "5edfcf79b35e145ec722",
+    "url": "/royal/build/static/js/3.5d8fa0d1.chunk.js"
+  },
+  {
+    "revision": "25ab7760a9d196e1bee67b6bdb56e1bd",
+    "url": "/royal/build/static/js/3.5d8fa0d1.chunk.js.LICENSE"
+  },
+  {
+    "revision": "13882212e0c2bce82cc6",
+    "url": "/royal/build/static/js/4.14caff01.chunk.js"
+  },
+  {
+    "revision": "fa5255f0f438d314f93d",
+    "url": "/royal/build/static/js/5.db2b1c52.chunk.js"
+  },
+  {
+    "revision": "405f8f9c81adc6800b52",
+    "url": "/royal/build/static/js/main.0c641d0a.chunk.js"
+  },
+  {
+    "revision": "e358c5069a8385830296",
+    "url": "/royal/build/static/js/runtime-main.a2e8ab54.js"
+  },
+  {
+    "revision": "b1a3498d3aa04b1714b10c4236446392",
+    "url": "/royal/build/static/media/construction.b1a3498d.png"
+  },
+  {
+    "revision": "10861a456f52ecfd4cc45013e78bfaa1",
+    "url": "/royal/build/static/media/r11.10861a45.png"
+  },
+  {
+    "revision": "f4511ec52e2e29e388d7c732fcc8c3f5",
+    "url": "/royal/build/static/media/template1.f4511ec5.png"
+  },
+  {
+    "revision": "adcca00874445505b568c42ead9e3224",
+    "url": "/royal/build/static/media/template2.adcca008.png"
+  },
+  {
+    "revision": "dd037aa5f8016accb8469e96625edabc",
+    "url": "/royal/build/static/media/template3.dd037aa5.png"
+  },
+  {
+    "revision": "c3e849356219fbb470c0a8a31de69b49",
+    "url": "/royal/build/static/media/template4.c3e84935.png"
+  },
+  {
+    "revision": "6b79a3100814e8430d1d04b3a44ef799",
+    "url": "/royal/build/static/media/wddding.6b79a310.png"
+  }
+]);
